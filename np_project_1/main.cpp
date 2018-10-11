5b8cefec2c4e979d2aba9f654b855cfbd00ac8d0
YuHsuan <sandy89876@gmail.com>
2018-10-11 13:47:03 +0800

    Initial Commit

---
+++ np_project_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  np_project_1
+//
+//  Created by 胡育旋 on 2018/10/11.
+//  Copyright © 2018 胡育旋. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
